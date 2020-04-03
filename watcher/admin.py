b33783c9de1207ceb9702e237898c2cafc278d74
Hebert <hebertjulio@users.noreply.github.com>
2020-04-03 10:00:30 -0300

    change technology field name to programming language and, model and admin adjustments

--- watcher/admin.py
+++ watcher/admin.py
@@ -5,20 +5,61 @@ from .models import Package, Release, Log
 
 @admin.register(Package)
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'technology', 'twitter_account', 'code_hosting', 'repository',)
-    search_fields = ('name', 'technology', 'twitter_account', 'code_hosting', 'repository',)
-    readonly_fields = ('created', 'modified',)
+    list_display = (
+        'name', 'programming_language', 'code_hosting', 'repository',
+        'twitter_account',
+    )
+    search_fields = (
+        'name', 'repository',
+    )
+    readonly_fields = (
+        'created', 'modified',
+    )
+    list_filter = (
+        'programming_language', 'code_hosting', 'twitter_account',
+    )
 
 
 @admin.register(Release)
 class ReleaseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'prerelease', 'published_at', 'status', 'package',)
-    search_fields = ('name', 'body', 'prerelease', 'published_at', 'status', 'package',)
-    readonly_fields = ('created', 'modified',)
+    list_display = (
+        'package', 'name', 'prerelease', 'published_at', 'status',
+    )
+    search_fields = (
+        'name', 'package',
+    )
+    readonly_fields = (
+        'created', 'modified',
+    )
+    list_filter = (
+        'prerelease', 'created', 'published_at', 'status',
+    )
+    autocomplete_fields = (
+        'package',
+    )
 
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
-    list_display = ('content_type', 'object_id', 'content_object', 'message',)
-    search_fields = ('content_type', 'object_id', 'content_object', 'message',)
-    readonly_fields = ('created', 'modified',)
+    list_display = (
+        'content_type', 'object_id', 'message', 'created',
+    )
+    search_fields = (
+        'content_type', 'object_id', 'message',
+    )
+    readonly_fields = (
+        'content_type', 'object_id', 'message',
+        'created', 'modified',
+    )
+    list_filter = (
+        'created',
+    )
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- watcher/models.py
+++ watcher/models.py
@@ -9,27 +9,34 @@ from model_utils import Choices
 
 
 class Package(TimeStampedModel):
-    TECHNOLOGY_CHOICES = Choices(
+    PROGRAMMING_LANGUAGE = Choices(
         ('python', 'python'),
-        ('node', 'node'),
+        ('javascript', 'javascript'),
     )
 
-    TWITTER_ACCOUNT_CHOICES = Choices(
+    TWITTER_ACCOUNT = Choices(
         ('pypackages', 'pypackages'),
     )
 
-    CODE_HOSTING_CHOICES = Choices(
+    CODE_HOSTING = Choices(
         ('github', 'github'),
     )
 
-    name = models.CharField(_('name'), max_length=100)
-    technology = models.CharField(
-        _('technology'), max_length=30, choices=TECHNOLOGY_CHOICES)
+    name = models.CharField(
+        _('name'), max_length=100
+    )
+    programming_language = models.CharField(
+        _('programming language'), max_length=30, choices=PROGRAMMING_LANGUAGE
+    )
     twitter_account = models.CharField(
-        _('twitter account'), max_length=30, choices=TWITTER_ACCOUNT_CHOICES)
+        _('twitter account'), max_length=30, choices=TWITTER_ACCOUNT
+    )
     code_hosting = models.CharField(
-        _('code hosting'), max_length=30, choices=CODE_HOSTING_CHOICES)
-    repository = models.CharField(_('repository'), max_length=200)
+        _('code hosting'), max_length=30, choices=CODE_HOSTING
+    )
+    repository = models.CharField(
+        _('repository'), max_length=200
+    )
 
     def __str__(self):
         return self.name
@@ -41,27 +48,38 @@ class Package(TimeStampedModel):
         verbose_name = _('package')
         verbose_name_plural = _('packages')
         unique_together = [
-            ['name', 'technology'],
+            ['name', 'programming_language'],
             ['code_hosting', 'repository'],
+        ]
+        ordering = [
+            'name',
         ]
 
 
 class Release(TimeStampedModel):
-    STATUS_CHOICES = Choices(
+    STATUS = Choices(
         ('new', _('new')),
         ('available', _('available')),
         ('tweeted', _('tweeted')),
         ('fail', _('fail')),
     )
 
-    name = models.CharField(_('name'), max_length=30)
-    body = models.TextField(_('body'), blank=True)
-    prerelease = models.BooleanField(_('prerelease'))
-    published_at = models.DateTimeField(_('published at'))
+    name = models.CharField(
+        _('name'), max_length=30
+    )
+    prerelease = models.BooleanField(
+        _('prerelease')
+    )
+    published_at = models.DateTimeField(
+        _('published at')
+    )
     status = models.CharField(
-        _('status'), max_length=30, db_index=True, choices=STATUS_CHOICES,
-        default=STATUS_CHOICES.new)
-    package = models.ForeignKey('Package', on_delete=models.CASCADE)
+        _('status'), max_length=30, db_index=True,
+        choices=STATUS, default=STATUS.new
+    )
+    package = models.ForeignKey(
+        'Package', on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
@@ -75,10 +93,28 @@ class Release(TimeStampedModel):
         unique_together = [
             ['name', 'package'],
         ]
+        ordering = [
+            'name',
+        ]
 
 
 class Log(TimeStampedModel):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-    message = models.TextField(_('message'))
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE
+    )
+    object_id = models.PositiveIntegerField(
+        _('object id')
+    )
+    content_object = GenericForeignKey(
+        'content_type', 'object_id'
+    )
+    message = models.TextField(
+        _('message')
+    )
+
+    class Meta:
+        verbose_name = _('log')
+        verbose_name_plural = _('logs')
+        ordering = [
+            '-created',
+        ]
