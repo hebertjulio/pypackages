7a6883738a90283b523b77c7ac6106ccc160d73a
Hebert <hebertjulio@users.noreply.github.com>
2020-04-05 08:49:39 -0300

    pip8 format

--- watcher/management/commands/tweet_releases.py
+++ watcher/management/commands/tweet_releases.py
@@ -1,9 +1,7 @@
 import sys
-import re
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from django.utils.dateparse import parse_datetime
 
 import tweepy
 
@@ -12,7 +10,7 @@ from ...models import Release
 
 class Command(BaseCommand):
     help = 'Tweet new releases.'
-    
+
     api = {}
     tweet_message = "#{} a new release was launched: {}"
 
@@ -22,8 +20,14 @@ class Command(BaseCommand):
         for language in ['python', 'javascript', 'css']:
             twitter_account = twitter_accounts.get(language)
             if twitter_account is not None:
-                auth = tweepy.OAuthHandler(twitter_account["API_KEY"], twitter_account["API_SECRET"])
-                auth.set_access_token(twitter_account["ACCESS_TOKEN"], twitter_account["ACCESS_TOKEN_SECRET"])
+                auth = tweepy.OAuthHandler(
+                    twitter_account["API_KEY"],
+                    twitter_account["API_SECRET"]
+                )
+                auth.set_access_token(
+                    twitter_account["ACCESS_TOKEN"],
+                    twitter_account["ACCESS_TOKEN_SECRET"]
+                )
                 self.api[language] = tweepy.API(auth)
 
     def handle(self, *args, **options):
@@ -40,11 +44,12 @@ class Command(BaseCommand):
         releases = Release.objects.filter(status=Release.STATUS.new)
         for release in releases:
             self.write_tweet(release)
-    
+
     def write_tweet(self, release):
         try:
             api = self.api[release.package.programming_language]
-            api.update_status(self.tweet_message.format(release.package.name, release.name))
+            api.update_status(self.tweet_message.format(
+                release.package.name, release.name))
             release.status = Release.STATUS.tweeted
         except Exception as e:
             release.status = Release.STATUS.fail
