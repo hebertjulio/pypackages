6513e908715190069b5fef7846b21459a0f03b50
Hebert <hebertjulio@users.noreply.github.com>
2020-04-05 08:53:22 -0300

    remove generic foreign key

--- watcher/models.py
+++ watcher/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
-
 from model_utils.models import TimeStampedModel
 from model_utils import Choices
 
