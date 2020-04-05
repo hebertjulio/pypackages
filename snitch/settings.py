cba0dc851e05bd48511d1eb542b67fa33cce8db7
Hebert <hebertjulio@users.noreply.github.com>
2020-04-05 11:52:21 -0300

    remove STATICFILES_DIRS

--- snitch/settings.py
+++ snitch/settings.py
@@ -119,10 +119,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 
 # https://docs.djangoproject.com/en/3.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development
 
