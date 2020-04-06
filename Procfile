26027e255567dc852a7682f789a761ed9b839f0f
Hebert <hebertjulio@users.noreply.github.com>
2020-04-06 08:32:19 -0300

    create clock file to scheduled jobs

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
+clock: python clock.py
 web: gunicorn snitch.wsgi --log-file -
---
+++ clock.py
@@ -0,0 +1,51 @@
+import sys
+import subprocess
+import time
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', hours=1)
+def watch_releases():
+    try:
+        print('watch_releases: starting')
+        start_time = time.time()
+        subprocess.check_output(['python', 'manage.py', 'watch_releases'])
+        print('watch_releases: finished in %s seconds' % (
+            time.time() - start_time))
+    except Exception:
+        print('watch_releases: failed')
+
+
+@sched.scheduled_job('interval', hours=1)
+def clean_recent_actions():
+    try:
+        print('clean_recent_actions: starting')
+        start_time = time.time()
+        subprocess.check_output(
+            ['python', 'manage.py', 'clean_recent_actions'])
+        print('clean_recent_actions: finished in %s seconds' % (
+            time.time() - start_time))
+    except Exception:
+        print('clean_recent_actions: failed')
+
+
+@sched.scheduled_job('interval', hours=1)
+def clearsessions():
+    try:
+        print('clearsessions: starting')
+        start_time = time.time()
+        subprocess.check_output(['python', 'manage.py', 'clearsessions'])
+        print('clearsessions: finished in %s seconds' % (
+            time.time() - start_time))
+    except Exception:
+        print('clearsessions: failed')
+
+
+try:
+    sched.start()
+except KeyboardInterrupt:
+    sys.exit(0)
