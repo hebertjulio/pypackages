8d8edca8116c33c7b2c68ead296d47ba110a09b2
Hebert <hebertjulio@users.noreply.github.com>
2020-04-06 08:28:30 -0300

    remove time executation print and log insert

--- recentactions/management/commands/clean_recent_actions.py
+++ recentactions/management/commands/clean_recent_actions.py
@@ -1,5 +1,3 @@
-import time
-
 from django.core.management.base import BaseCommand
 from django.contrib.admin.models import LogEntry
 
@@ -8,6 +6,4 @@ class Command(BaseCommand):
     help = 'Clean recent actions logging.'
 
     def handle(self, *args, **options):
-        start_time = time.time()
         LogEntry.objects.all().delete()
-        print("clean_recent_actions: %s seconds" % (time.time() - start_time))
