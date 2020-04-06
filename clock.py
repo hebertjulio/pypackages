63e8f8c74708ac3129c1f4f004662ae1e4237211
Hebert <hebertjulio@users.noreply.github.com>
2020-04-06 13:47:15 -0300

    change schedules

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', hours=24)
+@sched.scheduled_job('cron', hour=0, minute=0)
 def watch_releases():
     try:
         print('watch_releases: starting')
@@ -20,7 +20,7 @@ def watch_releases():
         print('watch_releases: failed')
 
 
-@sched.scheduled_job('interval', hours=1)
+@sched.scheduled_job('cron', hour=23, minute=0)
 def clean_recent_actions():
     try:
         print('clean_recent_actions: starting')
@@ -33,7 +33,7 @@ def clean_recent_actions():
         print('clean_recent_actions: failed')
 
 
-@sched.scheduled_job('interval', hours=1)
+@sched.scheduled_job('cron', hour=23, minute=30)
 def clearsessions():
     try:
         print('clearsessions: starting')
@@ -45,16 +45,19 @@ def clearsessions():
         print('clearsessions: failed')
 
 
-@sched.scheduled_job('interval', minutes=30)
-def tweet_releases():
-    try:
-        print('tweet_releases: starting')
-        start_time = time.time()
-        subprocess.check_output(['python', 'manage.py', 'tweet_releases'])
-        print('tweet_releases: finished in %s seconds' % (
-            time.time() - start_time))
-    except Exception:
-        print('tweet_releases: failed')
+for h in range(5, 23):
+    for m in [0, 30]:
+        @sched.scheduled_job('cron', hour=h, minute=m)
+        def tweet_releases():
+            try:
+                print('tweet_releases: starting')
+                start_time = time.time()
+                subprocess.check_output([
+                    'python', 'manage.py', 'tweet_releases'])
+                print('tweet_releases: finished in %s seconds' % (
+                    time.time() - start_time))
+            except Exception:
+                print('tweet_releases: failed')
 
 
 try:
