4eb5b664412073f6bc38a12f78d290b9e2fb097d
Hebert <hebertjulio@users.noreply.github.com>
2020-04-07 15:58:21 -0300

    added help text in release_regex field

--- watcher/models.py
+++ watcher/models.py
@@ -32,7 +32,10 @@ class Package(TimeStampedModel):
         _('repository name'), max_length=100
     )
     release_regex = models.CharField(
-        _('release regex'), max_length=100
+        _('release regex'), max_length=100,
+        help_text=(_(
+            'v2.4.5 or 2.4.5 => ^v?((\d+)(?:\.\d+)+)$ | '
+            'v2.4.5rc1 or 2.4.5rc1 => ^v?((\d+)(?:\.\d+)+\w*)$')),
     )
 
     def __str__(self):
