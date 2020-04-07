28906433667b765a654c019b421a43e5eb70c1d9
Hebert <hebertjulio@users.noreply.github.com>
2020-04-07 16:04:16 -0300

    added scape in help text

--- watcher/models.py
+++ watcher/models.py
@@ -34,8 +34,8 @@ class Package(TimeStampedModel):
     release_regex = models.CharField(
         _('release regex'), max_length=100,
         help_text=(_(
-            'v2.4.5 or 2.4.5 => ^v?((\d+)(?:\.\d+)+)$ | '
-            'v2.4.5rc1 or 2.4.5rc1 => ^v?((\d+)(?:\.\d+)+\w*)$')),
+            'v2.4.5 or 2.4.5 => ^v?((\\d+)(?:\\.\\d+)+)$ | '
+            'v2.4.5rc1 or 2.4.5rc1 => ^v?((\\d+)(?:\\.\\d+)+\\w*)$')),
     )
 
     def __str__(self):
