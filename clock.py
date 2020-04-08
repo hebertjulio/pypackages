3795a36b84a510d45a14ae2c8bcdd55eba2dbd71
Hebert <hebertjulio@users.noreply.github.com>
2020-04-08 08:04:06 -0300

    schedules adjustment

--- clock.py
+++ clock.py
@@ -8,56 +8,50 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', hour=0, minute=0)
+@sched.scheduled_job('interval', hours=6)
 def watch_releases():
     try:
-        print('watch_releases: starting')
         start_time = time.time()
         subprocess.check_output(['python', 'manage.py', 'watch_releases'])
-        print('watch_releases: finished in %s seconds' % (
+        print('watch_releases finished in %s seconds' % (
             time.time() - start_time))
-    except Exception:
-        print('watch_releases: failed')
+    except Exception as e:
+        print('watch_releases failed %s' % e)
 
 
-@sched.scheduled_job('cron', hour=23, minute=45)
+@sched.scheduled_job('interval', hours=24)
 def clean_recent_actions():
     try:
-        print('clean_recent_actions: starting')
         start_time = time.time()
         subprocess.check_output(
             ['python', 'manage.py', 'clean_recent_actions'])
-        print('clean_recent_actions: finished in %s seconds' % (
+        print('clean_recent_actions finished in %s seconds' % (
             time.time() - start_time))
-    except Exception:
-        print('clean_recent_actions: failed')
+    except Exception as e:
+        print('clean_recent_actions failed %s' % e)
 
 
-@sched.scheduled_job('cron', hour=23, minute=45)
+@sched.scheduled_job('interval', hours=24)
 def clearsessions():
     try:
-        print('clearsessions: starting')
         start_time = time.time()
         subprocess.check_output(['python', 'manage.py', 'clearsessions'])
-        print('clearsessions: finished in %s seconds' % (
+        print('clearsessions finished in %s seconds' % (
             time.time() - start_time))
-    except Exception:
-        print('clearsessions: failed')
+    except Exception as e:
+        print('clearsessions failed %s' % e)
 
 
-for h in range(5, 24):
-    for m in [0, 30]:
-        @sched.scheduled_job('cron', hour=h, minute=m)
-        def tweet_releases():
-            try:
-                print('tweet_releases: starting')
-                start_time = time.time()
-                subprocess.check_output([
-                    'python', 'manage.py', 'tweet_releases'])
-                print('tweet_releases: finished in %s seconds' % (
-                    time.time() - start_time))
-            except Exception:
-                print('tweet_releases: failed')
+@sched.scheduled_job('interval', minutes=30)
+def tweet_releases():
+    try:
+        start_time = time.time()
+        subprocess.check_output([
+            'python', 'manage.py', 'tweet_releases'])
+        print('tweet_releases finished in %s seconds' % (
+            time.time() - start_time))
+    except Exception as e:
+        print('tweet_releases failed %s' % e)
 
 
 try:
