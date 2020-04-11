a61787a9b2ef9aa0a16b6681ef5939e52de0c86e
Hebert <hebertjulio@users.noreply.github.com>
2020-04-11 12:16:04 -0300

    FIX #36 Audit required: Starting a process with a partial executable path BAN-B607

--- clock.py
+++ clock.py
@@ -8,36 +8,36 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', hour=12)
+@sched.scheduled_job('interval', hours=6)
 def watch_releases():
     try:
         start_time = time.time()
         subprocess.call(
-            'python manage.py watch_releases; exit 0', shell=True)
+            'python ${PWD}/manage.py watch_releases; exit 0', shell=True)
         print('watch_releases finished in %s seconds' % (
             time.time() - start_time))
     except Exception as e:
         print('watch_releases failed %s' % e)
 
 
-@sched.scheduled_job('interval', hours=1)
+@sched.scheduled_job('interval', hours=6)
 def clean_recent_actions():
     try:
         start_time = time.time()
         subprocess.call(
-            'python manage.py clean_recent_actions; exit 0', shell=True)
+            'python ${PWD}/manage.py clean_recent_actions; exit 0', shell=True)
         print('clean_recent_actions finished in %s seconds' % (
             time.time() - start_time))
     except Exception as e:
         print('clean_recent_actions failed %s' % e)
 
 
-@sched.scheduled_job('interval', hours=1)
+@sched.scheduled_job('interval', hours=6)
 def clearsessions():
     try:
         start_time = time.time()
         subprocess.call(
-            'python manage.py clearsessions; exit 0', shell=True)
+            'python ${PWD}/manage.py clearsessions; exit 0', shell=True)
         print('clearsessions finished in %s seconds' % (
             time.time() - start_time))
     except Exception as e:
@@ -49,7 +49,7 @@ def tweet_releases():
     try:
         start_time = time.time()
         subprocess.call(
-            'python manage.py tweet_releases; exit 0', shell=True)
+            'python ${PWD}/manage.py tweet_releases; exit 0', shell=True)
         print('tweet_releases finished in %s seconds' % (
             time.time() - start_time))
     except Exception as e:
