8d3ca6aa02d9abb62f0be8e7454481b15d4abd5a
Hebert <hebertjulio@users.noreply.github.com>
2020-05-31 18:42:17 -0300

    Cria classe client BigQuery e adiciona pacotes para acesso a api do google

---
+++ packages/clients/bigquery.py
@@ -0,0 +1,27 @@
+from google.cloud import bigquery
+
+
+class BigQuery:
+
+    __QUERY = """
+        SELECT file.project AS name, COUNT(*) AS downloads
+        FROM `the-psf.pypi.downloads*`
+        WHERE file.project IN ({projects})
+        AND _TABLE_SUFFIX
+            BETWEEN FORMAT_DATE(
+                '%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY))
+            AND FORMAT_DATE('%Y%m%d', CURRENT_DATE())
+        GROUP BY file.project
+    """
+
+    def __init__(self):
+        self.client = bigquery.Client()
+
+    def get_downloads(self, projects):
+        if not isinstance(projects, list):
+            raise ValueError
+        projects = ','.join(['\'%s\'' % v for v in projects])
+        query = self.__QUERY.format(projects=projects)
+        query_job = self.client.query(query)
+        result = query_job.result()
+        return result
